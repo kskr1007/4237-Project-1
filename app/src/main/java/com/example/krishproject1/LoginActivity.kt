46a315e991ea4bf420ddcfb9b3d7b1f74960cdc1
kskr1007 <krishop@outlook.com>
2026-02-12 13:48:59 -0500

    copied over login screen from week 5 activity.

---
+++ app/src/main/java/com/example/krishproject1/LoginActivity.kt
@@ -0,0 +1,83 @@
+package com.example.krishproject1
+
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun LogInScreen(modifier: Modifier = Modifier) {
+
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    // for toast messages
+    val context = LocalContext.current
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "Login screen",
+            fontSize = 40.sp
+        )
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        TextField(
+            value = username,
+            onValueChange = { newValue ->
+                username = newValue
+            },
+            label = { Text("Enter Username") },
+            modifier = Modifier.padding(8.dp)
+        )
+
+        TextField(
+            value = password,
+            onValueChange = { newValue ->
+                password = newValue
+            },
+            label = { Text("Enter Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.padding(8.dp)
+        )
+
+        Button(
+            onClick = {
+                Toast.makeText(context, "You logged in", Toast.LENGTH_LONG).show()
+            }, enabled = checkUsernameAndPassword(username,password)
+        ) {
+            Text("Login")
+        }
+    }
+}
+
+fun checkUsernameAndPassword(username: String, password: String): Boolean {
+    return username.isNotBlank() && password.isNotBlank()
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPreview() {
+    LogInScreen()
+}

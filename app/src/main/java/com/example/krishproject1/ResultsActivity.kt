08529600a79cac02638584ac7330f0eb4e22c5a2
kskr1007 <krishop@outlook.com>
2026-03-03 18:14:06 -0500

    cleaned code

--- app/src/main/java/com/example/krishproject1/ResultsActivity.kt
+++ app/src/main/java/com/example/krishproject1/ResultsActivity.kt
@@ -30,7 +30,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -203,6 +202,7 @@ fun ResultsScreen(userQuery: String, sourceId: String, sourceName: String, horiz
                 }
             }
         }
+
         // if loading is true, show a spinning UI element
         // I got this code from internet
         // due to adding this UI feature, I had to wrap the column in a box due to alignment issues
